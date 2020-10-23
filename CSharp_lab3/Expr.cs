f2140faea66d1bb0263c8bf1ba86803a0ecd1d23
Elisey <shemetov.elisey@gmail.com>
2020-10-23 21:05:11 +0700

    initial commit

---
+++ CSharp_lab3/Expr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp_lab3
+{
+   public abstract class Expr
+   {
+      public abstract double Compute(IReadOnlyDictionary<string, double> variableValues);
+      public abstract IEnumerable<string> Variables { get; protected set; }
+      public abstract bool IsConstant { get; }
+      public abstract bool IsPolynom { get; }
+   }
+}
