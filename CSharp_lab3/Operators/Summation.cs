a10654d74b2a4109630b02fb4f3c9097aa53569b
Elisey <shemetov.elisey@gmail.com>
2020-11-03 18:54:51 +0700

    add Operators folder

---
+++ CSharp_lab3/Operators/Summation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CSharp_lab3
+{
+	public class Summation : BinaryOperation
+	{
+		public Summation(Expr lhs, Expr rhs) : base(lhs, rhs)
+		{ }
+
+		public override bool IsConstant => throw new NotImplementedException();
+
+		public override bool IsPolynom => throw new NotImplementedException();
+
+		protected override double Calculate(double lhs, double rhs) => lhs + rhs;
+	}
+}
